ea33b32fc6dbdba3d4a7eb7334f3ed0b2cdea4af
Francis Wibisono <francis.wibisono@ui.ac.id>
2023-01-25 15:25:20 +0700

    feat: basic movement functionality

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,68 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script>
+	import { draggable } from '@neodrag/svelte';
+
+	let innerWidth = 0;
+	$: gridCellWidth = innerWidth / columns;
+
+	let columns = 50;
+	let rows = 5;
+	let snapToGrid = true;
+	let position = { x: 1415, y: 129 };
+
+	$: cells = Array(columns * rows - 1).fill(0);
+
+	//value control
+	$: rows < 1 ? (rows = 1) : rows;
+	$: rows > 200 ? (rows = 200) : rows;
+
+	$: columns < 1 ? (columns = 1) : columns;
+	$: columns > 200 ? (columns = 200) : columns;
+
+
+</script>
+
+<label><input type="number" bind:value={rows} />rows</label>
+<label><input type="number" bind:value={columns} />columns</label>
+<label><input type="checkbox" bind:checked={snapToGrid} />snap</label>
+the position is now in {position.x} {position.y}
+
+<div class="primary-grid bg-blue-200 w-fit h-fit" id="grids" style="--numCols: {columns}">
+	<div
+		use:draggable={{
+			grid: snapToGrid ? [gridCellWidth, gridCellWidth] : undefined,
+			bounds: '#grids',
+			position,
+			onDrag: ({ offsetX, offsetY }) => {
+				position = { x: offsetX, y: offsetY };
+			}
+		}}
+		class="select-none cursor-pointer bg-red-500 my-auto rounded-full token flex"
+	>
+		<span class="mx-auto my-auto"> a </span>
+	</div>
+	{#each cells as i}
+		<div class="grid-cell border-black border-2" style="--numCols: {columns}" />
+	{/each}
+</div>
+
+<svelte:window bind:innerWidth />
+
+<style>
+	.grid-cell {
+		height: calc(100vw / var(--numCols));
+		width: calc(100vw / var(--numCols));
+		text-align: center;
+		justify-content: center;
+		display: flex;
+	}
+
+	.primary-grid {
+		display: grid;
+		grid-template-columns: repeat(var(--numCols), 1fr);
+	}
+
+	.token {
+		height: calc(100vw / var(--numCols));
+		width: calc(100vw / var(--numCols));
+	}
+</style>
--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,8 +1,13 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ['./src/**/*.{html,js,svelte,ts}'],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
-}
+	content: ['./src/**/*.{html,js,svelte,ts}'],
+	theme: {
+		extend: {
+			gridTemplateColumns: {
+				// Simple 16 column grid
+				50: 'repeat(50, minmax(0, 1fr))'
+			}
+		}
+	},
+	plugins: []
+};
